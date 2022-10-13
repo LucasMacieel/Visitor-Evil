4dec9738c318a2741619c740296aef2bcbedc806
Xande2k01 <68670366+AlexandreRodolfo@users.noreply.github.com>
2022-10-13 16:51:02 -0300

    Delete WeaponAnimatorAux.cs

--- WeaponAnimatorAux.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class WeaponAnimatorAux : MonoBehaviour {
-	[Range(0,1)]public float leftHandOpen, leftIndexOut;
-	[Range(0,1)]public float rightHandOpen, rightIndexOut;
-}
